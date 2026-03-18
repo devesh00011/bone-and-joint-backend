f01f1ce0538662d1238a1d39466654b6c441e320
devesh00011 <tarunmehra80790@gmail.com>
2026-03-18 11:05:29 +0530

    admin dashboard origin add

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://www.boneandjointhospital.co.in"
+  "https://www.boneandjointhospital.co.in",
+  "https://admin.boneandjointhospital.co.in"
 ];
 
 app.use(
